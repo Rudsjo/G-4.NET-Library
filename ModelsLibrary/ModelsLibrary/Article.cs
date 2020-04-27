589efe8cb38de0f2fccb7aba17bb582ab3998896
stimp <stimpon@gmail.com>
2020-04-28 01:59:08 +0200

    Added user model and article model

---
+++ ModelsLibrary/ModelsLibrary/Article.cs
@@ -0,0 +1,37 @@
+﻿namespace ModelsLibrary
+{
+    /// <summary>
+    /// Required namespaces
+    /// </summary>
+    #region Namespaces
+    using System;
+    #endregion
+
+    /// <summary>
+    /// This is a model of an article in
+    /// the library's system
+    /// </summary>
+    public class Article
+    {
+        /// <summary>
+        /// Title of the article
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// This article's ISBN
+        /// </summary>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// Author of the article
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// This article's release date
+        /// </summary>
+        public DateTime Published { get; set; }
+
+    }
+}
---
+++ ModelsLibrary/ModelsLibrary/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ModelsLibrary
+{
+    /// <summary>
+    /// This model contains all necessary information
+    /// about a user. It is supposed to be a return
+    /// object from the repository.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The user's username
+        /// </summary>
+        public string Username { get; set; }
+
+    }
+}
