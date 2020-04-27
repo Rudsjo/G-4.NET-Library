bbde21af10798d1c5d562690ef1b4b21eade7a1a
stimp <stimpon@gmail.com>
2020-04-28 01:56:43 +0200

    Added categories and usertypes to the core library

---
+++ Library.Core/Library.Core/DataModels/Enums.cs
@@ -0,0 +1,56 @@
+﻿namespace Library.Core
+{
+    /// <summary>
+    /// Categories for articles
+    /// </summary>
+    public enum ArticleCategories
+    {
+        /// <summary>
+        /// A horror article
+        /// </summary>
+        Horror,
+
+        /// <summary>
+        /// A comedy article
+        /// </summary>
+        Comedy,
+
+        /// <summary>
+        /// An action article
+        /// </summary>
+        Action,  
+
+        /* 
+          
+            Add more later
+
+         */
+    }
+
+    /// <summary>
+    /// Usertypes
+    /// </summary>
+    public enum UserTypes
+    {
+        /// <summary>
+        /// Someone that can do everything
+        /// </summary>
+        Administrator = 0,
+
+        /// <summary>
+        /// A librarian ? ?
+        /// </summary>
+        Librarian = 1,
+
+        /// <summary>
+        /// A logged in user
+        /// </summary>
+        User = 2,
+
+        /// <summary>
+        /// A visitor that is not logged in
+        /// </summary>
+        Visitor = 3
+    }
+
+}
