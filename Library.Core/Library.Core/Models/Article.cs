33081810dcd5d3675cb4fc089a246a3d6b86830b
stimpon <56868514+stimpon@users.noreply.github.com>
2020-05-01 00:06:56 +0200

    Moved User and Article models to the core library

---
+++ Library.Core/Library.Core/Models/Article.cs
@@ -0,0 +1,43 @@
+﻿namespace ModelsLibrary
+{
+    /// <summary>
+    /// Required namespaces
+    /// </summary>
+    #region Namespaces
+    using System;
+    using Library.Core;
+    #endregion
+
+    /// <summary>
+    /// This is a model of an article in
+    /// the library's system
+    /// </summary>
+    public class Article
+    {
+        /// <summary>
+        /// Title of the article
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// This article's ISBN
+        /// </summary>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// Author of the article
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The category of this article>
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// This article's release date
+        /// </summary>
+        public DateTime Published { get; set; }
+
+    }
+}
---
+++ Library.Core/Library.Core/Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ModelsLibrary
+{
+    /// <summary>
+    /// Required namespaces
+    /// </summary>
+    #region Namespaces
+    using Library.Core;
+    #endregion
+
+    /// <summary>
+    /// This model contains all necessary information
+    /// about a user. It is supposed to be a return
+    /// object from the repository.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The user's username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The user type <see cref="UserTypes"/>
+        /// </summary>
+        public UserTypes Type { get; set; }
+    }
+}
