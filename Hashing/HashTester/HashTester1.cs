035b88774499b353eb292ae892a71aecc960cca9
stimp <stimpon@gmail.com>
2020-05-07 15:25:31 +0200

    Added HashWithSalt function

--- Hashing/HashTester/HashTester1.cs
+++ Hashing/HashTester/HashTester1.cs
@@ -22,16 +22,34 @@ namespace HashTester
         public void CreatePasswordAndSaltTest()
         {
             // Create a passwordhash.
-            var result1 = CreatePasswordAndSalt("password123".ToSecureString());
+            var result1 = GenerateSaltAndHashPassword("password123".ToSecureString());
 
             // Create a passwordhash with the same password.
-            var result2 = CreatePasswordAndSalt("password123".ToSecureString());
+            var result2 = GenerateSaltAndHashPassword("password123".ToSecureString());
 
             // Compare their values.
             // - They should not have the same hash.
             // - They should not have the same salt.
             Assert.IsTrue((result1.Item1.ToUnsecureString() != result2.Item1.ToUnsecureString()) &&
                            result1.Item2.CompareTo(result2.Item2) != 0);
+        }
+
+        /// <summary>
+        /// Compares 2 passwords that should have the same value.
+        /// </summary>
+        [Test]
+        public void ComparePasswordsTest()
+        {
+            // Create a passwordhash.
+            var result1 = GenerateSaltAndHashPassword("password123".ToSecureString());
+
+            // Create a passwordhash with the same salt.
+            var result2 = HashPasswordWithSpecificSalt("password123".ToSecureString(), result1.Item2);
+
+            // Compare their values.
+            // The 2 hashes should be the same.
+            Assert.AreEqual( result1.Item1.ToUnsecureString(), 
+                             result2.ToUnsecureString());
         }
     }
 }
--- Hashing/Hashing/HashFunctions.cs
+++ Hashing/Hashing/HashFunctions.cs
@@ -17,21 +17,22 @@
     public class HashFunctions
     {
         /// <summary>
-        ///     
-        ///     This function is intended to be used when a new
-        ///     user is created. A random salt is generated and
-        ///     the provided password appends the salt and then
-        ///     gets hashed with SHA256 before it is sent to the
-        ///     database.
-        /// 
+        ///     Hashes a password with a salt.
+        ///     <usage>
+        ///         This function is intended to be used when a new
+        ///         user is created. A random salt is generated and
+        ///         the provided password appends the salt and then
+        ///         gets hashed with SHA256 before it is sent to the
+        ///         database.
+        ///     </usage>
         /// </summary>
-        ///     <param name="Password">The password that will be hashed.</param>
+        ///     <param name="Password">The <see cref="SecureString"/> that will be hashed.</param>
         /// <returns> 
         /// 
         ///    ( PASSWORD-HASH, SALT ) 
         ///    
         /// </returns>
-        public static ( SecureString, string ) CreatePasswordAndSalt(SecureString Password)
+        public static ( SecureString, string ) GenerateSaltAndHashPassword(SecureString Password)
         {
             // Check if provided string is null.
             if (Password == null)
@@ -60,10 +61,39 @@
              */ 
             var PasswordHash = Convert.ToBase64String ( Hasher.ComputeHash ( 
                                                         Encoding.Default.GetBytes ( 
-                                                                                    Password + SaltBase64
+                                                                                    Password.ToUnsecureString() + SaltBase64
                                                                                   ))).ToSecureString();
             // Return the password-hash and the salt.
             return (PasswordHash, SaltBase64);
+        }
+
+        /// <summary>
+        ///     Hashes a password with a privided salt.
+        ///     <usage>
+        ///         This functin is intended to be used when a user 
+        ///         attempts to login, the database will provide the 
+        ///         provided users's salt (if it exists) and the 
+        ///         client hashes the input password with that salt 
+        ///         before it is sent of to the database for comparison.
+        ///     </usage>
+        /// </summary>
+        /// <param name="InputPassword">Provided <see cref="SecureString"/></param>
+        /// <param name="SaltBase64"> The salt that was provided from the database,
+        ///                           Represented as a Base64 string.
+        ///                           </param>
+        /// <returns></returns>
+        public static SecureString HashPasswordWithSpecificSalt(SecureString InputPassword, string SaltBase64)
+        {
+            // Create a SHA256 hasher.
+            var Hasher = SHA256.Create();
+
+            // - Append salt to the password.
+            // - Compute hash.
+            // - Return the bytes as a SecureString.
+            return Convert.ToBase64String ( Hasher.ComputeHash (
+                                            Encoding.Default.GetBytes (
+                                                                        InputPassword.ToUnsecureString() + SaltBase64
+                                                                     ))).ToSecureString();           
         }
 
     }
